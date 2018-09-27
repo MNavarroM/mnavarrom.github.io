9671e4589e47c0d0e7ea16bc3a62e70e7c793f86
Mario Navarro <navarromario1@gmail.com>
2018-09-27 21:41:09 +0200

    Primer despliegue repositorio DAW

---
+++ Cliente/Tema 1 - Arquitectura y herramientas/ejercicios1-7/ejercicio3/codigo.js
@@ -0,0 +1,10 @@
+/**
+ * Crear un array llamado meses y que almacene el nombre de los doce meses del año.
+ * Mostrar por pantalla los doce nombres utilizando la función alert().
+ * @author Mario Navarro Madrid
+ */
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo",
+"Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+for (let i = 0; i < meses.length; i++) {
+    alert(meses[i]);
+}
