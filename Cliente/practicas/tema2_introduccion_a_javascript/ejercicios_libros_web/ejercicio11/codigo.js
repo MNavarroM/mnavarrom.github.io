92e7fc51ddb05498e8964e0ec98fac140c1bb0b9
Mario Navarro <navarromario1@gmail.com>
2018-10-03 09:17:55 +0200

    Probando GitKraken

--- Cliente/practicas/tema2_introduccion_a_javascript/ejercicios_libros_web/ejercicio11/codigo.js
+++ Cliente/practicas/tema2_introduccion_a_javascript/ejercicios_libros_web/ejercicio11/codigo.js
@@ -3,24 +3,24 @@
  * 1.Número de enlaces de la página
  * 2.Dirección a la que enlaza el penúltimo enlace
  * 3.Numero de enlaces que enlazan a http://prueba
- * 4. Número de enlaces del tercer párrafo
+ * 4.Número de enlaces del tercer párrafo
  * @author Mario Navarro Madrid
  */
 
- {
+{
 
     function numEnlaces() {
         document.getElementById("numenlaces").textContent = "La web tiene " + enlaces.length + " enlaces";
     }
 
-    function penultimoEnlace(){
-        document.getElementById("penultimoenlace").textContent = "El penúltimo enlace apunta a " + enlaces[enlaces.length-2].href;
+    function penultimoEnlace() {
+        document.getElementById("penultimoenlace").textContent = "El penúltimo enlace apunta a " + enlaces[enlaces.length - 2].href;
     }
 
-    function numEnlacesPrueba(){
-        let numEnlaces=0;
+    function numEnlacesPrueba() {
+        let numEnlaces = 0;
         for (let i = 0; i < enlaces.length; i++) {
-            if(enlaces[i].href == "http://prueba/"){
+            if (enlaces[i].href == "http://prueba/") {
                 numEnlaces++;
             }
         }
@@ -30,7 +30,7 @@
     function numEnlacesTercerParrafo() {
         let parrafos = document.getElementsByTagName("p");
         let enlaces = parrafos[2].getElementsByTagName("a");
-        document.getElementById("enlacestercerparrafo").textContent ="En el tercer párrafo tenemos " + enlaces.length + " enlaces";
+        document.getElementById("enlacestercerparrafo").textContent = "En el tercer párrafo tenemos " + enlaces.length + " enlaces";
     }
 
     function init() {
@@ -41,6 +41,6 @@
         numEnlacesTercerParrafo();
     }
 
-    window.addEventListener("load",init);
+    window.addEventListener("load", init);
 
- }
+}
