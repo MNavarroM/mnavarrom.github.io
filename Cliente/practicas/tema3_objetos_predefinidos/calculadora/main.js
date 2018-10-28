77b10d3f28d77053873b9696fe27bbaec11bdb82
Mario Navarro <navarromario1@gmail.com>
2018-10-28 22:39:23 +0100

    Actualizacion calculador

--- Cliente/practicas/tema3_objetos_predefinidos/calculadora/main.js
+++ Cliente/practicas/tema3_objetos_predefinidos/calculadora/main.js
@@ -6,6 +6,7 @@
 {
     function init() {
         createCalculadora();
+        display();
     }
 
     function createCalculadora() {
@@ -20,12 +21,15 @@
         cuerpoCalculadora.style.width ="240px";
         cuerpoCalculadora.style.margin ="0px auto";
         input = document.createElement("input");
+        input.value=0;
         input.type = "text";
+        input.id ="display";
+        //input.readOnly = true;
         input.style.width = "236px";
         input.style.height = "30px";
         main.appendChild(cuerpoCalculadora);
         cuerpoCalculadora.appendChild(input);
-        let botones = ["CE","<-","%","+","7","8","9","-","4",
+        let botones = ["CE","DEL","%","+","7","8","9","-","4",
         "5","6","X","1","2","3","/","0" ,"+/-",",","="];
         let contador = 0;
         for (let i = 0; i < 5; i++) {
@@ -34,11 +38,100 @@
                 let button = document.createElement("button");
                 button.style.width = "60px";
                 button.style.height = "50px";
+                button.className = "botones";
+                button.value = botones[contador]
                 button.textContent =botones[contador++];
                 fila.appendChild(button);
             }
             cuerpoCalculadora.appendChild(fila);
         }
+    }
+
+    function display() {
+        input = document.getElementById("display");
+        btns = document.getElementsByClassName("botones");
+        Array.prototype.forEach.call(btns, element => {
+            element.addEventListener("click",function () {
+                switch (element.value) {
+                    case "DEL":
+                        input.value = input.value.substr(0,input.value.length-1);
+                    break;
+                    case "CE":
+                        input.value = 0;
+                        break;
+                    case "0":
+                        if(input.value!=0)
+                            input.value += 0;
+                        break;
+                    case "1":
+                        if(input.value==0)
+                            input.value = 1;
+                        else
+                            input.value += 1;
+                        break;
+                    case "2":
+                        if(input.value==0)
+                            input.value = 2;
+                        else
+                            input.value += 2;
+                        break;
+                    case "3":
+                        if(input.value==0)
+                            input.value = 3;
+                        else
+                            input.value += 3;
+                        break;
+                    case "4":
+                        if(input.value==0)
+                            input.value = 4;
+                        else
+                            input.value += 4;
+                        break;
+                    case "5":
+                        if(input.value==0)
+                            input.value = 5;
+                        else
+                            input.value += 5;
+                        break;
+                    case "6":
+                        if(input.value==0)
+                            input.value = 6;
+                        else
+                            input.value += 6;
+                        break;
+                    case "7":
+                        if(input.value==0)
+                            input.value = 7;
+                        else
+                            input.value += 7;
+                        break;
+                    case "8":
+                        if(input.value==0)
+                            input.value = 8;
+                        else
+                            input.value += 8;
+                        break;
+                    case "9":
+                        if(input.value==0)
+                            input.value = 9;
+                        else
+                            input.value += 9;
+                        break;
+                    case "+/-":
+                        if(input.value>0)
+                            input.value = -Math.abs(input.value);
+                        else
+                            input.value = Math.abs(input.value);
+                        break;
+                    case ",":
+                        if(!input.value.includes(","))
+                            input.value += ",";
+                        break;
+                }
+            });
+        }
+        );
+
     }
 
     window.addEventListener("load",init);
