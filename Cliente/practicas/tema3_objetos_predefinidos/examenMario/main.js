c9d7c61f4c60f738e75a859172a82e2085518492
Mario Navarro <navarromario1@gmail.com>
2018-11-21 08:39:20 +0100

    Subida reentrega, aún sin terminar, no me manden issues-2 ;

---
+++ Cliente/practicas/tema3_objetos_predefinidos/examenMario/main.js
@@ -0,0 +1,20 @@
+{
+
+    function init() {
+        document.getElementById("a1").addEventListener("click",function (event) {
+            event.preventDefault();
+            window.open("ejercicio1.html","_self");
+        });
+        document.getElementById("a2").addEventListener("click",function (event) {
+            event.preventDefault();
+            window.open("ejercicio2.html","_self");
+        });
+        document.getElementById("a3").addEventListener("click",function (event) {
+            event.preventDefault();
+            window.open("ejercicio3.html","_self");
+        });        
+    }
+
+
+    document.addEventListener("DOMContentLoaded",init);
+}
