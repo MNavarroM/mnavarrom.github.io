2753672295eab6fa275e20a86b8b2dbc35ca5e98
Mario Navarro <navarromario1@gmail.com>
2018-11-25 20:34:44 +0100

    Calculadora con objeto literal

--- Cliente/practicas/tema3_objetos_predefinidos/calculadora/main.js
+++ Cliente/practicas/tema3_objetos_predefinidos/calculadora/main.js
@@ -4,148 +4,128 @@
  * @author Mario Navarro Madrid
  */
 {
-  let num;
-  let operador = "";
-  let banderaOperacion = false;
+
+  let calculadora = {
+    num: 0,
+    operador : "",
+    banderaOperacion : false,
+    botones : ["CE", "DEL", "%", "+", "7", "8", "9", "-", "4", "5", "6", "X", "1", "2", "3", "/", "0", "+/-", ",", "="],
+    input : [],
+    crearCalculadora(){
+      let nombre = document.createElement("h1");
+      nombre.textContent = "Calculadora - Mario Navarro Madrid";
+      let main = document.createElement("main");
+      let cuerpoCalculadora = document.createElement("div");
+      cuerpoCalculadora.id = "cuerpoCalculadora";
+      let input = document.createElement("input");
+      input.value = 0;
+      input.type = "text";
+      input.id = "display";
+      main.appendChild(nombre);
+      main.appendChild(cuerpoCalculadora);
+      cuerpoCalculadora.appendChild(input);
+      document.body.appendChild(main);
+      let contador = 0;
+      for (let i = 0; i < 5; i++) {
+        let fila = document.createElement("div");
+        for (let j = 0; j < 4; j++) {
+          let button = document.createElement("button");
+          button.className = "botones";
+          button.value = calculadora.botones[contador];
+          button.textContent = calculadora.botones[contador++];
+          button.addEventListener("click", calculadora.funcionalidad);
+          fila.appendChild(button);
+        }
+        cuerpoCalculadora.appendChild(fila);
+        calculadora.input = document.getElementById("display");
+      }
+    },
+
+    funcionalidad: function() {
+      if (!isNaN(parseInt(this.value))) {
+        if(calculadora.input.value == "Infinity")
+          calculadora.input.value = this.value;
+        if (calculadora.banderaOperacion)
+          calculadora.input.value = 0;
+        if (calculadora.input.value == 0 && !calculadora.input.value.includes('.'))
+          calculadora.input.value = parseFloat(this.value);
+        else
+          calculadora.input.value += parseFloat(this.value);
+        calculadora.banderaOperacion = false;
+        } else {
+          if ( this.value == "+" || this.value == "-" || this.value == "/" || this.value == "X")
+            calculadora.operar();
+          calculadora.comprobarBoton(this.value);
+          if(this.value != "+-" && this.value != "," && this.value != "CE" && this.value != "DEL")
+            calculadora.banderaOperacion = true;
+        }
+      },
+
+      comprobarBoton: function(value) {
+        if(value=="+" || value=="-" || value=="X" || value=="/"){
+          calculadora.num = parseFloat(calculadora.input.value);
+        }
+        switch (value) {
+          case "+":
+            calculadora.operador = "+";
+            break;
+          case "-":
+            calculadora.operador = "-";
+            break;
+          case "X":
+            calculadora.operador = "X";
+            break;
+          case "/":
+            calculadora.operador = "/";
+            break;
+          case "DEL":
+            calculadora.input.value = calculadora.input.value.substr(0, calculadora.input.value.length - 1);
+            if (calculadora.input.value == "-" || calculadora.input.value.length == 0)
+            calculadora.input.value = 0;
+            break;
+          case "CE":
+            calculadora.input.value = 0;
+            calculadora.operador ="";
+            break;
+          case "+/-":
+            calculadora.input.value = parseFloat(calculadora.input.value)*-1;
+            break;
+          case ",":
+            if (!calculadora.input.value.includes("."))
+              calculadora.input.value += ".";
+            break;
+          case "%":
+            calculadora.input.value = parseFloat(calculadora.input.value) / 100;
+          case "=":
+            calculadora.operar();
+            break;
+        }
+      },
+
+      operar: function() {
+        if(calculadora.operador!=""){
+          switch (calculadora.operador) {
+            case "+":
+              calculadora.input.value = parseFloat(calculadora.input.value) + calculadora.num;
+              break;
+            case "-":
+              calculadora.input.value = calculadora.num - parseFloat(calculadora.input.value);
+              break;
+            case "X":
+              calculadora.input.value = parseFloat(calculadora.input.value) * calculadora.num;
+              break;
+            case "/":
+              calculadora.input.value = calculadora.num / parseFloat(calculadora.input.value);
+              break;
+          }
+        }
+        calculadora.operador = "";
+      }
+  }
 
   function init() {
-    createCalculadora();
-    let btns = Array.from(document.getElementsByClassName("botones"));
-    input = document.getElementById("display");
-    btns.forEach(element => {
-      element.addEventListener("click", funcionalidad);
-    });
+    calculadora.crearCalculadora();
   }
 
-  function createCalculadora() {
-    let nombre = document.createElement("h1");
-    nombre.textContent = "Calculadora - Mario Navarro Madrid";
-    let main = document.createElement("main");
-    let cuerpoCalculadora = document.createElement("div");
-    cuerpoCalculadora.id = "cuerpoCalculadora";
-    let input = document.createElement("input");
-    input.value = 0;
-    input.type = "text";
-    input.id = "display";
-    main.appendChild(nombre);
-    main.appendChild(cuerpoCalculadora);
-    cuerpoCalculadora.appendChild(input);
-    document.body.appendChild(main);
-    let botones = [
-      "CE",
-      "DEL",
-      "%",
-      "+",
-      "7",
-      "8",
-      "9",
-      "-",
-      "4",
-      "5",
-      "6",
-      "X",
-      "1",
-      "2",
-      "3",
-      "/",
-      "0",
-      "+/-",
-      ",",
-      "="
-    ];
-    let contador = 0;
-    for (let i = 0; i < 5; i++) {
-      let fila = document.createElement("div");
-      for (let j = 0; j < 4; j++) {
-        let button = document.createElement("button");
-        button.className = "botones";
-        button.value = botones[contador];
-        button.textContent = botones[contador++];
-        fila.appendChild(button);
-      }
-      cuerpoCalculadora.appendChild(fila);
-    }
-  }
-
-  function funcionalidad() {
-  if (!isNaN(parseInt(this.value))) {
-    if(input.value == "Infinity")
-      input.value = this.value;
-    if (banderaOperacion)
-      input.value = 0;
-    if (input.value == 0 && !input.value.includes('.'))
-      input.value = parseFloat(this.value);
-    else
-      input.value += parseFloat(this.value);
-    banderaOperacion = false;
-    } else {
-      if ( this.value == "+" || this.value == "-" || this.value == "/" || this.value == "X")
-        operar();
-      comprobarBoton(this.value);
-      if(this.value != "+-" && this.value != "," && this.value != "CE" && this.value != "DEL")
-        banderaOperacion = true;
-    }
-  }
-
-  function comprobarBoton(value) {
-    if(value=="+" || value=="-" || value=="X" || value=="/"){
-      num = parseFloat(input.value);
-    }
-    switch (value) {
-      case "+":
-        operador = "+";
-        break;
-      case "-":
-        operador = "-";
-        break;
-      case "X":
-        operador = "X";
-        break;
-      case "/":
-        operador = "/";
-        break;
-      case "DEL":
-        input.value = input.value.substr(0, input.value.length - 1);
-        if (input.value == "-" || input.value.length == 0)
-        input.value = 0;
-        break;
-      case "CE":
-        input.value = 0;
-        operador ="";
-        break;
-      case "+/-":
-        input.value = parseFloat(input.value)*-1;
-        break;
-      case ",":
-        if (!input.value.includes("."))
-          input.value += ".";
-        break;
-      case "%":
-        input.value = parseFloat(input.value) / 100;
-      case "=":
-        operar();
-        break;
-    }
-  }
-
-  function operar() {
-    if(operador!=""){
-      switch (operador) {
-        case "+":
-          input.value = parseFloat(input.value) + num;
-          break;
-        case "-":
-          input.value = num - parseFloat(input.value);
-          break;
-        case "X":
-          input.value = parseFloat(input.value) * num;
-          break;
-        case "/":
-          input.value = num / parseFloat(input.value);
-          break;
-      }
-    }
-  }
-
-  window.addEventListener("load", init);
+  document.addEventListener("DOMContentLoaded", init);
 }
