64696030d8c88f6b57ed3e8ee37ef63b8b5e419f
Mario Navarro <navarromario1@gmail.com>
2018-12-18 09:35:36 +0100

    update gestor

---
+++ Diseño interfaces/practicas/gestorTareasResponsive/main.js
@@ -0,0 +1,27 @@
+{
+  let btnAdd, btnExit;
+  let modal;
+  let notas;
+
+  function init() {
+    /*notas = document.getElementsByClassName("nota");
+    arrayNotas = Array.from(notas);
+    console.log(notas);
+    for (let i = 0; i < arrayNotas.length; i++) {
+        notas[i].addEventListener("click",function () {
+            notas.removeChild(notas.childNodes[i]);
+        })
+    }*/
+    btnAdd = document.getElementById("btnNota");
+    btnExit = document.getElementById("btnCancel");
+    modal = document.getElementById("modal");
+    btnAdd.addEventListener("click", function() {
+      modal.style.display = "block";
+    });
+    btnExit.addEventListener("click", function() {
+      modal.style.display = "none";
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", init);
+}
