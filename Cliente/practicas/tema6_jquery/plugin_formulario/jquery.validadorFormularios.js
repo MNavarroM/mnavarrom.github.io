486e649b50304a4f6b039dd8be53b19a778ae8da
Mario Navarro <navarromario1@gmail.com>
2019-02-25 18:42:19 +0100

    plugin formulario

---
+++ Cliente/practicas/tema6_jquery/plugin_formulario/jquery.validadorFormularios.js
@@ -0,0 +1,68 @@
+(function ($) {
+    let validador = {
+        nombre: [/^([a-zA-Z]{3,}\s?)$/,
+            "El nombre debe tener al menos tres carácteres."
+        ],
+        apellidos: [/^([a-zA-Z]{3,}\s?){1,3}$/,
+            "Debes introducir al menos un apellido de tres carácteres."
+        ],
+        email: [/^[^áéíóúÁÉÍÓÚ ()<>@,;:"\[\]\.ç%&]+@[^áéíóúÁÉÍÓÚ ()<>@,;:"\[\]\.ç%&]+\.[^áéíóúÁÉÍÓÚ ()<>@,;:"\[\]\.ç%&]{2,4}$/, "El email no cumple el formato xxxxxx@xxxx.xxx"],
+
+        mensaje: [/^^.{20,}$/, "El mensaje debe contener minimo 20 cárateres."],
+
+        validarRegex(valor, expresion) {
+            if (!validador[expresion][0].test(valor))
+                return false;
+            return true;
+        },
+    }
+
+    $.fn.validadorFormularios = function () {
+        let inputError = [];
+        $("input[type=text], textarea").blur(function (e) {
+            e.preventDefault();
+            let tipoRegex = $(this).attr("tipo");
+            let valor = $(this).val();
+            if (validador.validarRegex(valor, tipoRegex)) {
+                $(this).css({
+                    color: "green",
+                    border: "1px solid green"
+                });
+            } else {
+                $(this).css({
+                    color: "red",
+                    border: "1px solid red"
+                });
+                inputError.push($(this));
+            }
+        });
+
+        $("input[type=text], textarea").focus(function (e) {
+            e.preventDefault();
+            $(this).css({
+                color: "black",
+                border: "2px solid #ffD3D7"
+            });
+        });
+
+        $(this).submit(function (e) {
+            e.preventDefault();
+            inputError = [];
+            let $inputs = $("input[type=text], textarea");
+            $inputs.blur();
+            console.log(inputError);
+            if (inputError.length > 0)
+                inputError[0].focus();
+            else {
+                $.ajax({
+                    url: "info.txt",
+                    dataType: "text",
+                    success: function (response) {
+                        $("textarea").val(response);
+                    }
+                });
+            }
+        });
+    }
+        return this;
+    }(jQuery));
