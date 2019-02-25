bd4d51b09481d6ff1f1d5e8fd2fd604048b51c74
Mario Navarro <navarromario1@gmail.com>
2019-02-25 10:02:09 +0100

    fix cargar fichero

--- Cliente/practicas/tema7_ajax/cargando_fichero/main.js
+++ Cliente/practicas/tema7_ajax/cargando_fichero/main.js
@@ -2,21 +2,29 @@ $(function() {
   get("index2.html");
   $("button").mousedown(function() {
     get($("#cajaTexto").val());
+    $("#codigoPeticion").html("");
   });
 });
 
 function get($url) {
   $.get($url, function(data, textStatus, jqXHR) {
+    let estados = "";
     $("#pagina").text(data);
+  }).done(function(data, textStatus, jqXHR) {
+    $("#descripcionRespuesta").text(jqXHR.statusText);
+    $("#codigoRespuesta").text(jqXHR.status);
+    $("#codigoPeticion").append("Done<br/>");
+    console.log(jqXHR.readyState)
+  }).fail(function (data, textStatus, jqXHR) {
+    $("#descripcionRespuesta").text("404");
+    $("#codigoRespuesta").text("Not Found");
+    $("#codigoPeticion").append("Fail<br/>");
+    $("#pagina").text("");
+    console.log(jqXHR.readyState)
   }).always(function(data, textStatus, jqXHR) {
     $("#descripcionRespuesta").text(jqXHR.statusText);
     $("#codigoRespuesta").text(jqXHR.status);
-    $("#descripcionPeticion").text(textStatus);
-    $("#codigoPeticion").text(jqXHR.readyState);
-  }).fail(function () {
-    $("#descripcionRespuesta").text("404");
-    $("#codigoRespuesta").text("Not Found");
-    $("#descripcionPeticion").text(textStatus);
-    $("#codigoPeticion").text(jqXHR.readyState);
+    $("#codigoPeticion").append("Always<br/>");
+    console.log(jqXHR.readyState)
   });
 }
